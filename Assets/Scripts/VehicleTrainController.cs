322a340b681db9d5182f2a583243da12641f3f53
Seung-Ki Kim <62886544+Seung-Ki-Kim@users.noreply.github.com>
2024-10-15 23:15:50 +0900

    열차 움직임 구현 (물리 엔진 사용)
    열차의 움직임을 구현한다.
    - AddForce( ) 메서드를 사용해 직접 힘을 가하는, 현실적인 방식을 적용했다.
    - 최적화 문제가 우려되어 추후 구현 방식을 수정할 필요가 있어보인다.
    - 물리적인 방식을 사용하다보니, 가속도 값 조정을 통해 자연스러운 정차와 재출발 등을 구현할 수 있었다.
    - 열차의 무게와 가속도, 최고 속도 값은 실제 전철 제원을 참고함.
    (30톤, 15키로, 80키로)
    -FixedJoint로 연결되어 있기 때문에 Mass Scale 값도 함께 올려줘야 한다.
    (안 올리면 열차가 움직이다가 무중력 상태에 빠진 것처럼 이리저리 떠다님)
    - 열차는 최고 속도 값을 넘지 않는다.
    - 가속도를 음수로 만들면 후진한다.

--- Assets/Scripts/VehicleTrainController.cs
+++ Assets/Scripts/VehicleTrainController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class VehicleTrainController : MonoBehaviour {
     [Header("Vehicle Component")]
@@ -13,13 +14,20 @@ public class VehicleTrainController : MonoBehaviour {
     [Space(25f)]
     
     [Header("Vehicle Setting")]
-    [SerializeField] private float vehicleCurrentSpeed;
-    [SerializeField] private float vehicleAcceleration;
-    private Vector3 vehiclePosition;
-
-
+    [SerializeField] private float vehicleCurrentSpeed;     // n km/h
+    [SerializeField] private float vehicleMaxSpeed;         // 80 km/h
+    [SerializeField] private float vehicleAcceleration;     // 15 km/h
+    
+    private Transform vehicleTransform;
+    private Rigidbody vehicleRigidbody;
+    
+    
     private void Init() {
-        this.vehiclePosition = this.engineCar.transform.position;
+        this.vehicleMaxSpeed /= 3.6f;       // km/h -> m/s
+        this.vehicleAcceleration /= 3.6f;   // km/h -> m/s
+        
+        this.vehicleTransform = this.engineCar.transform;
+        this.vehicleRigidbody = this.engineCar.GetComponent<Rigidbody>();
     }
 
     private void Awake() {
@@ -35,6 +43,17 @@ public class VehicleTrainController : MonoBehaviour {
     }
 
     private void FixedUpdate() {
+        VehicleEngineControl();
+    }
+
+    private void VehicleEngineControl() {
+        this.vehicleCurrentSpeed = this.vehicleRigidbody.velocity.magnitude;    // Current Speed (velocity Magnitude)
         
+        if (this.vehicleCurrentSpeed < this.vehicleMaxSpeed) {
+            this.vehicleRigidbody.AddForce(this.vehicleTransform.forward * (this.vehicleAcceleration * this.vehicleRigidbody.mass * this.jointCars.Count), ForceMode.Force);    // Acceleration(m/s) * deltaTime * Mass 
+        }
+        else {
+            this.vehicleRigidbody.velocity = this.vehicleRigidbody.velocity.normalized * this.vehicleMaxSpeed;
+        }
     }
 }
