2fd2d90c0671b7457ad675b342b1dad962e1d130
Seung-Ki Kim <kimsk200012@gmail.com>
2024-11-24 00:21:09 +0900

    확장 메뉴 펼치고 접기 기능 구현 중
    확장 메뉴를 스와이프하여 펼치고 접는 기능 구현 중.
    
    기존에 있었던 버그는 해결
    - 이미 다 접혔는데/펼쳤는데 더 접거나/펼치거나 하면 한 번 더 펼치거나 접어야 접히고/펼쳐지는 현상
    - 일단 버그는 잡았으나, 값을 변경하고 컨디션을 검사하는 특성 때문에 제대로 동작이 안되는 중.
    - 값을 변경하고 컨디션에 대입하고 있다는 순서를 참고하여 수정할 것.

--- Assets/Scripts/GameController/GameControlObjectDragDropManager.cs
+++ Assets/Scripts/GameController/GameControlObjectDragDropManager.cs
@@ -5,7 +5,25 @@ public class GameControlObjectDragDropManager : MonoBehaviour, IPointerDownHandl
     [SerializeField] private RectTransform targetRectTransform;
     [SerializeField] private Canvas targetCanvas;
     [SerializeField] private Animator animator;
-    
+
+    // private float currentDelta;
+    private Vector2 swipeStartPosition;
+    private Vector2 swipeEndPosition;
+    private float swipeDirection;
+    private int swipeLevel;
+
+
+    private void Init() {
+        this.swipeDirection = 0f;
+        this.swipeLevel = 0;
+
+        this.animator.SetBool("isEndLevel", true);
+        this.animator.SetFloat("direction", this.swipeDirection);
+    }
+
+    private void Awake() {
+        Init();
+    }
     
     public void OnPointerDown(PointerEventData eventData) {
         Debug.Log("OnPointerDown");
@@ -13,17 +31,37 @@ public class GameControlObjectDragDropManager : MonoBehaviour, IPointerDownHandl
 
     public void OnBeginDrag(PointerEventData eventData) {
         Debug.Log("OnBeginDrag");
+        this.swipeStartPosition = (eventData.position / this.targetCanvas.scaleFactor);
     }
 
     public void OnDrag(PointerEventData eventData) {
         Debug.Log("OnDrag");
-        this.targetRectTransform.anchoredPosition = new Vector2(0, Mathf.Clamp((this.targetRectTransform.anchoredPosition.y + (eventData.delta.y / this.targetCanvas.scaleFactor)), -146.25f, 1176.45f));
+        // this.currentDelta = (eventData.delta.y / this.targetCanvas.scaleFactor);
+        // this.targetRectTransform.anchoredPosition = new Vector2(0, Mathf.Clamp((this.targetRectTransform.anchoredPosition.y + this.currentDelta), -140f, 1190f));
     }
     
     public void OnEndDrag(PointerEventData eventData) {
         Debug.Log("OnEndDrag");
         
-        // TODO: MENU DRAG ANI
+        // TODO: MENU DRAG
+        this.swipeEndPosition = (eventData.position / this.targetCanvas.scaleFactor);
+        this.swipeDirection = (this.swipeEndPosition - this.swipeStartPosition).normalized.y;
+
+        Debug.Log(this.swipeDirection);
         
+        switch (this.swipeDirection) {
+            case > 0:
+                this.swipeLevel -= 1;
+                break;
+            case < 0:
+                this.swipeLevel += 1;
+                break;
+        }
+        
+        Debug.Log(this.swipeLevel);
+        
+        this.animator.SetFloat("direction", this.swipeDirection);
+        this.animator.SetBool("isEndLevel", (this.swipeLevel is -1 or 3));  // TODO: 번호가 이상하게 바뀌고 있음;
+        this.animator.SetTrigger("onSwipe");
     }
 }
