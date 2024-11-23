cbbdbc848b04ea5cafaec215ea069c6d04957600
Seung-Ki Kim <kimsk200012@gmail.com>
2024-11-23 21:20:16 +0900

    확장 메뉴 UI 레이아웃 편집 & 메뉴 UI 조작 스크립트 수정 중
    레이아웃이 틀어진 부분 수정
    
    메뉴 UI 조작 부분 스크립트 수정 중.

--- Assets/Scripts/GameController/GameControlObjectDragDropManager.cs
+++ Assets/Scripts/GameController/GameControlObjectDragDropManager.cs
@@ -2,17 +2,13 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class GameControlObjectDragDropManager : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IDragHandler, IEndDragHandler {
-    [SerializeField] private RectTransform draggingObjectRectTransform;
-    [SerializeField] private Canvas canvas;
-    [SerializeField] private Animator pivotAnimator;
-    [SerializeField] private AnimationClip[] pivotAnimationClips;
-    
-    private float _achoredPosition;
+    [SerializeField] private RectTransform targetRectTransform;
+    [SerializeField] private Canvas targetCanvas;
+    [SerializeField] private Animator animator;
     
     
     public void OnPointerDown(PointerEventData eventData) {
         Debug.Log("OnPointerDown");
-        this._achoredPosition = eventData.position.y;
     }
 
     public void OnBeginDrag(PointerEventData eventData) {
@@ -21,21 +17,13 @@ public class GameControlObjectDragDropManager : MonoBehaviour, IPointerDownHandl
 
     public void OnDrag(PointerEventData eventData) {
         Debug.Log("OnDrag");
-        // this.draggingObjectRectTransform.anchoredPosition = new Vector2(0, Mathf.Clamp((this.draggingObjectRectTransform.anchoredPosition.y + (eventData.delta.y / this.canvas.scaleFactor)), -146.25f, 1176.45f));
+        this.targetRectTransform.anchoredPosition = new Vector2(0, Mathf.Clamp((this.targetRectTransform.anchoredPosition.y + (eventData.delta.y / this.targetCanvas.scaleFactor)), -146.25f, 1176.45f));
     }
     
     public void OnEndDrag(PointerEventData eventData) {
         Debug.Log("OnEndDrag");
         
-        // MENU DRAG ANI
-        var direction = (eventData.position.y - this._achoredPosition);
+        // TODO: MENU DRAG ANI
         
-        // TODO: 접힌 상태에서 한 번 더 접거나, 펼친 상태에서 한 번 더 펼치면 이전 제스처가 다음 행동 때 중첩으로 적용되는 버그 발생
-        if (direction < 0) {    // DOWN
-            this.pivotAnimator.SetTrigger("Down");
-        }
-        else if (direction > 0) {   // UP
-            this.pivotAnimator.SetTrigger("Up");
-        }
     }
 }
