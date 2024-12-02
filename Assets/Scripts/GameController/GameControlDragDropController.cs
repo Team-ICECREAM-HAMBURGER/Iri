ea09206ec13ccb476ad116d6c108a46e725e7569
Seung-Ki Kim <kimsk200012@gmail.com>
2024-12-02 22:48:55 +0900

    증거품 아이템 드래그&드롭 기능 구현
    1. 진행사항
    - ‘증거품 수사 화면’에서 서류나 열차 티켓 등의 아이템을 드래그&드롭할 수 있도록 구현.
    - transform.SetAsLastSibling();를 OnPointerDown() 메서드에서 호출하여 클릭한 오브젝트가 최상단에 위치하도록 Hierarchy 창에서의 순서를 자동으로 변경.
    - GameControlDragDropController.cs  스크립트 컴포넌트를 사용.
        -- 포인터 클릭/드래그/클릭 해제 등 포인터를 다루는 기능을 구현하기 위한 인터페이스와 가상 메서드를 묶어둔 GameControlPointerManager.cs 가상 클래스를 상속하는 클래스.
        -- 해당 클래스는 아래 참조를 사용하여 OnDrag() 메서드 내에서 캔버스 사이즈에 맞게 delta 값을 보정, 마우스 위치를 캔버스 사이즈와 무관하게 정확히 추적할 수 있도록 하여 드래그&드롭 기능을 구현한다.
            ---  targetRectTransform: 드래그&드롭하게 될 아이템의 RectTransform 참조.
            --- targetCanvas: 아이템이 위치해 있는 Canvas 참조.
    
    ///
    
    2. 문제점
    - 오브젝트가 화면 밖으로 벗어나면 다시 불러올 방법이 없다.
        -- 아이러니하게도 Windows 운영체제에서도 종종 겪는 문제점.
        -- 오브젝트를 처음 위치로 되돌려버리는 버튼을 하나 만들어서 배치하는 것도 방법이 될 수 있다.
            --- 버튼 디자인 및 배치가 거슬리지 않아야 한다.
                ---- ‘검문사항' 항목을 버튼으로 만들어서 클릭하면 오브젝트 위치를 초기화하도록 구현한다면?
                ---- 반납이 끝나면 어차피 검문이 끝났다는 의미이며, 검문 도중 다른 사람을 검문을 할 일은 없으므로 버튼으로 구현해도 큰 문제가 되지는 않을 것이라 생각.
            --- 레퍼런스가 되는 <Papers, Please>는 이 문제점을 어떻게 극복했는가?
        -- 드래그&드롭 기능을 사용할 수 있는 범위 자체를 좌표로 지정하고 싶으나, 증거품 수사 화면의 사이즈를 어떻게 추적할 것인지 문제점이 남아있다.
            --- 모바일 플랫폼을 지원한다면 기기 별로 화면 사이즈가 모두 다를텐데, 좌표값을 직접 넣어버리는 하드 코딩은 권장되지 않는다.
            --- 증거품 수사 화면 사이즈를 어떻게 수식으로 계산할 것인가?
            --- 증거품 수사 화면 사이즈를 지정해준다면, ‘활동 영역'의 기준은 무엇인가? (마우스 포인터? 각 오브젝트의 원점?)
    
    ///
    
    3. 중요사항
    - 수사할 증거품 아이템을 새로 추가하면 GameControlDragDropController.cs 스크립트 컴포넌트를 추가해주면 된다.
    
    ///
    
    4. 다음 단계
    - 오브젝트가 화면 밖으로 넘어갔을 경우, 오브젝트를 클릭할 수 없게 되는 문제 해결
    - ‘휴대식 검문 전산기'를 접고 펼치는 기능 구현
    - ‘검문 보고서’ 서류에 도장을 찍는 기능 구현
    - 검문 증거 아이템 신규 제작

--- Assets/Scripts/GameController/GameControlDragDropController.cs
+++ Assets/Scripts/GameController/GameControlDragDropController.cs
@@ -1,27 +1,19 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
-using UnityEngine.Serialization;
 
 public class GameControlDragDropController : GameControlPointerManager {
-    [Space(25f)]
-    
-    [SerializeField] private float anchorPositionMin;
-    [SerializeField] private float anchorPositionMax;
-    
-    private float currentDelta;
-    
-    
     public override void OnPointerDown(PointerEventData eventData) {
+        transform.SetAsLastSibling();
     }
-
+    
     public override void OnBeginDrag(PointerEventData eventData) {
     }
 
     public override void OnDrag(PointerEventData eventData) {
-        this.currentDelta = (eventData.delta.y / this.targetCanvas.scaleFactor);
-        this.targetRectTransform.anchoredPosition = new Vector2(this.targetRectTransform.anchoredPosition.x, Mathf.Clamp((this.targetRectTransform.anchoredPosition.y + this.currentDelta), this.anchorPositionMin, this.anchorPositionMax));
+        this.targetRectTransform.anchoredPosition = new Vector2(this.targetRectTransform.anchoredPosition.x + (eventData.delta.x / this.targetCanvas.scaleFactor), 
+            this.targetRectTransform.anchoredPosition.y + (eventData.delta.y / this.targetCanvas.scaleFactor));
     }
-
+    
     public override void OnEndDrag(PointerEventData eventData) {
     }
 }
