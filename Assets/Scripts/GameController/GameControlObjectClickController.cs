842971057dea4f6df4fbb6681b808b7ebbbd9794
Seung-Ki Kim <62886544+Seung-Ki-Kim@users.noreply.github.com>
2024-12-21 14:11:13 +0900

    UI 오브젝트 뒤에 있는 오브젝트 클릭까지 인식하는 문제 해결
    EventSystem.current.IsPointerOverGameObject() 메서드를 사용.
    - pointer가 UI 오브젝트 위에 있으면 True, 아니면 False를 반환.
    - if 문을 사용해 GameControlObjectClickController.cs의 OnSelect() 메서드 최상단에 return 문 배치. (주석 참고)

--- Assets/Scripts/GameController/GameControlObjectClickController.cs
+++ Assets/Scripts/GameController/GameControlObjectClickController.cs
@@ -1,8 +1,13 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
 
 public class GameControlObjectClickController : MonoBehaviour {
     private void OnSelect() {
+        if (EventSystem.current.IsPointerOverGameObject()) {    // UI 오브젝트 클릭 시 true
+            return;
+        }
+        
         Ray ray = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
 
         if (Physics.Raycast(ray, out RaycastHit hit)) {
