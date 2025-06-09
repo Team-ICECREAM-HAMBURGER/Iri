ea3a8d3c0d26e0b117949e0fc74af0a7e6fac9ad
Seung-Ki Kim <62886544+Seung-Ki-Kim@users.noreply.github.com>
2025-06-09 15:30:52 +0900

    열차를 부모 오브젝트가 제어하도록 수정
    열차의 움직임을 부모 오브젝트(최상단)가 제어함.

--- Assets/Scripts/VehicleTrain/VehicleTrainBehaviour.cs
+++ Assets/Scripts/VehicleTrain/VehicleTrainBehaviour.cs
@@ -1,25 +1,15 @@
 using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
 
 public class VehicleTrainBehaviour : MonoBehaviour { 
-    [Header("Train Components")] 
-    [SerializeField] private GameControlTypeManager.VehicleTrainType vehicleType;
-    [Space(10f)]
-    [SerializeField] private GameObject engineCar;
-    [SerializeField] private List<GameObject> jointCars;
-    [Space(10f)]
     [SerializeField] private float vehicleMaxSpeed;
     [SerializeField] private float vehicleAcceleration;
     [SerializeField] private float vehicleCurrentSpeed;
+    [SerializeField] private int jointCars;
     
     [Space(25f)]
     
-    [Header("System Components")]
     [SerializeField] private TrafficStateBehaviour trafficStateBehaviour;
-    
-    public List<Vector3> JointCarResetPositions { get; private set; }
-    public Vector3 EngineCarResetPosition { get; private set; }
     
     private Transform vehicleTransform;
     private Rigidbody vehicleRigidbody;
@@ -27,15 +17,12 @@ public class VehicleTrainBehaviour : MonoBehaviour {
     
     private void Init() {
         // Train Component Init //
-        this.vehicleTransform = this.engineCar.transform;
-        this.vehicleRigidbody = this.engineCar.GetComponent<Rigidbody>();
+        this.vehicleTransform = this.gameObject.transform;
+        this.vehicleRigidbody = this.gameObject.GetComponent<Rigidbody>();
         
         this.vehicleMaxSpeed /= 3.6f;       // km/h -> m/s
         this.vehicleAcceleration /= 3.6f;   // km/h -> m/s
-        
-        this.EngineCarResetPosition = this.engineCar.transform.localPosition;
-        this.JointCarResetPositions = this.jointCars.Select(car => car.transform.localPosition).ToList();
-        
+       
         // Traffic Light Event Listener //
         this.trafficStateBehaviour.OnTrafficUpdateIdle.AddListener(Idle);
         this.trafficStateBehaviour.OnTrafficUpdateMove.AddListener(Move);
@@ -47,18 +34,18 @@ public class VehicleTrainBehaviour : MonoBehaviour {
     }
 
     private void Idle() {
-        TrainInfoMonitorUpdate(GameControlTypeManager.TrafficState.IDLE);
+        // TrainInfoMonitorUpdate(GameControlTypeManager.TrafficState.IDLE);
         this.vehicleCurrentSpeed = 0f;
     }
     
     private void Move() {
-        TrainInfoMonitorUpdate(GameControlTypeManager.TrafficState.MOVE);
+        // TrainInfoMonitorUpdate(GameControlTypeManager.TrafficState.MOVE);
         this.vehicleCurrentSpeed = this.vehicleRigidbody.linearVelocity.magnitude;
         
         if (this.vehicleCurrentSpeed < this.vehicleMaxSpeed) {
             this.vehicleRigidbody.AddForce(
                 this.vehicleTransform.forward 
-                * (this.vehicleAcceleration * this.vehicleRigidbody.mass * this.jointCars.Count), ForceMode.Force);
+                * (this.vehicleAcceleration * this.vehicleRigidbody.mass * this.jointCars), ForceMode.Force);
         }
         else {
             this.vehicleRigidbody.linearVelocity 
@@ -67,20 +54,20 @@ public class VehicleTrainBehaviour : MonoBehaviour {
     }
     
     private void Stop() {
-        TrainInfoMonitorUpdate(GameControlTypeManager.TrafficState.STOP);
+        // TrainInfoMonitorUpdate(GameControlTypeManager.TrafficState.STOP);
         this.vehicleCurrentSpeed = this.vehicleRigidbody.linearVelocity.magnitude;
         
         if (Vector3.Dot(this.vehicleTransform.forward, this.vehicleTransform.up) >= 0.1f) {
             this.vehicleRigidbody.AddForce(
                 -this.vehicleTransform.forward 
-                * (this.vehicleAcceleration * this.vehicleRigidbody.mass * this.jointCars.Count), ForceMode.Force);
+                * (this.vehicleAcceleration * this.vehicleRigidbody.mass * this.jointCars), ForceMode.Force);
         }
         else {
             this.vehicleCurrentSpeed = 0f;
         }
     }
     
-    private void TrainInfoMonitorUpdate(GameControlTypeManager.TrafficState trafficState) {
-        TrainInfoMonitorBehaviour.OnTrafficStatusUpdate.Invoke(this.vehicleType, trafficState);
-    }
+    // private void TrainInfoMonitorUpdate(GameControlTypeManager.TrafficState trafficState) {
+    //     TrainInfoMonitorBehaviour.OnTrafficStatusUpdate.Invoke(this.vehicleType, trafficState);
+    // }
 }
